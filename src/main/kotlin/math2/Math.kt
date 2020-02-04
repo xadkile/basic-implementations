7a790f4318d1c8175831704ffdece11c14000d43
Unknown <xadkile@gmail.com>
2020-02-04 11:17:52 +0700

    set iteration limit for square root function

--- src/main/kotlin/math2/Math.kt
+++ src/main/kotlin/math2/Math.kt
@@ -5,23 +5,22 @@ import kotlin.math.abs
 fun main() {
 }
 
-fun squareRoot(x: Double, difLimit: Double): Double {
-    var re = x/7
+fun squareRoot(x: Double, difLimit: Double, maxIterationCount: Int): Double {
+    var re = x / 7
     var dif = (re * re - x) / (2 * re)
     var iterationCount = 0
-    while (abs(dif) >= difLimit) {
+    while ((abs(dif) >= difLimit).and(iterationCount <= maxIterationCount)) {
         re -= dif
         dif = (re * re - x) / (2 * re)
         iterationCount++
     }
-    println(iterationCount)
     return re
 }
 
-fun squareRoot(x: Number, difLimit: Double): Double {
-    return squareRoot(x.toDouble(), difLimit)
+fun squareRoot(x: Number, difLimit: Double, maxIterationCount: Int): Double {
+    return squareRoot(x.toDouble(), difLimit, maxIterationCount)
 }
 
 fun squareRoot(x: Number): Double {
-    return squareRoot(x, 0.00000000001)
+    return squareRoot(x, 0.00000000001, 20)
 }
